2bd0db142c6821581350b7c3a2df4902b379bc3e
erkkmohanty <er.kkmohanty@gmail.com>
2018-10-07 15:39:09 +0530

    permutation

---
+++ Permutation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Permutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Permutation("Ki", string.Empty);
+            Console.ReadLine();
+        }
+
+        static void Permutation(string input, string prefix)
+        {
+            if (input.Length == 0)
+                Console.WriteLine(prefix);
+            else
+            {
+                for(int index = 0; index < input.Length; index++)
+                {
+                    String remainderString = input.Substring(0, index) + input.Substring(index+1);
+                    Permutation(remainderString, prefix + input[index]);
+                }
+            }
+        }
+    }
+}
